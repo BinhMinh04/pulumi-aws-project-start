0c158c4860a8569fac516b1aca003c3193a92d93
Minh Nguyen <nguyendangbinhminh16@gmail.com>
2026-02-28 22:15:23 +0700

    Deploy s3

--- __main__.py
+++ __main__.py
@@ -1,10 +1,47 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+config = pulumi.Config()
+env = config.get("environment") or "dev"
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# 1. Tạo bucket
+bucket = aws.s3.BucketV2("my-bucket",
+    bucket=f"my-test-{env}-bucket-binhminh",
+    tags={
+        "Environment": env,
+        "ManagedBy": "Pulumi",
+    }
+)
+
+# 2. Bật versioning
+versioning = aws.s3.BucketVersioningV2("my-bucket-versioning",
+    bucket=bucket.id,
+    versioning_configuration=aws.s3.BucketVersioningV2VersioningConfigurationArgs(
+        status="Enabled"
+    )
+)
+
+# 3. Bật encryption
+encryption = aws.s3.BucketServerSideEncryptionConfigurationV2("my-bucket-encryption",
+    bucket=bucket.id,
+    rules=[aws.s3.BucketServerSideEncryptionConfigurationV2RuleArgs(
+        apply_server_side_encryption_by_default=aws.s3.BucketServerSideEncryptionConfigurationV2RuleApplyServerSideEncryptionByDefaultArgs(
+            sse_algorithm="AES256"
+        )
+    )]
+)
+
+# 4. Block public access
+public_access = aws.s3.BucketPublicAccessBlock("my-bucket-public-access",
+    bucket=bucket.id,
+    block_public_acls=True,
+    block_public_policy=True,
+    ignore_public_acls=True,
+    restrict_public_buckets=True,
+)
+
+# Exports
+pulumi.export("bucket_name", bucket.id)
+pulumi.export("bucket_arn", bucket.arn)
